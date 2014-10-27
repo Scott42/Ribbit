56efd4bd4cbf19221a5049a78361b24c41b24b99
Scott Shepard <scott.appdev@outlook.com>
2014-10-27 20:37:55 +1300

    Created an error alert dialog when user details are blank.

--- app/src/main/java/nz/net/thistle/ribbit/RibbitApplication.java
+++ app/src/main/java/nz/net/thistle/ribbit/RibbitApplication.java
@@ -13,9 +13,5 @@ public class RibbitApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, "tCYkbg9eI4CkjMO3wAUFFYvZWFF8WstgjP88Vdom", "7BBkmRPjTXLcl5L0JgqJEJ28w0EhR9cyJxhKTutu");
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
     }
 }
